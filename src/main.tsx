32f866d05fce265f92297480ca65723376bf9d24
mmssoo2866 <mmooss2866@gmail>
2026-08-22 02:13:51 +0300

    Deployment: trigger fresh clean build and clear Vercel cache

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import {StrictMode} from 'react';
+﻿import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
 import './index.css';
@@ -8,3 +8,4 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 );
+// Re-deploy cleanup: 08/22/2026 02:13:51
